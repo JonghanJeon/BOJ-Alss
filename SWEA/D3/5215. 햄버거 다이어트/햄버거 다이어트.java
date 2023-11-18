2d6f15655b4bb81e233172803fe1021eeabfcec7
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2023-11-19 00:49:08 +0900

    [D3] Title: 햄버거 다이어트, Time: 286 ms, Memory: 29,964 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution
+{
+	static int N;
+	static int L;
+	static int answer;
+	static int[][]  arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int test=1; test<=T; test++) {
+			N = sc.nextInt();
+			L = sc.nextInt();
+			
+			arr = new int[N][2];
+			
+			for(int i=0; i<N; i++) {
+				arr[i][0] = sc.nextInt();
+				arr[i][1] = sc.nextInt();
+			}
+			
+			Arrays.sort(arr, (o1, o2) -> {
+				return o2[0] - o1[0];
+			});
+			
+			answer = 0;
+			
+			a(0, 0, 0);
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("#").append(test).append(" ").append(answer);
+			System.out.println(sb.toString());	
+		}
+	}
+	
+	static void a (int start, int sumFlavor, int sumCal) {
+		if(sumCal > L) { // 전체 칼로리가 높으면 리턴
+			return;
+		}
+		if(start == N) { // 제일 깊이 들어갔다면, 칼로리 확인해서 바꿔주고 리턴
+			if(sumCal <= L) {
+				if(sumFlavor > answer) {
+					answer = sumFlavor;
+				}
+			}
+			return;
+		}
+		if(sumCal <= L) { // 칼로리가 적다면 바꿔주기
+			if(sumFlavor > answer) {
+				answer = sumFlavor;
+			}
+		}
+		a(start+1, sumFlavor, sumCal); // 이번꺼 제꾸고 다음꺼 가는거
+		a(start+1, sumFlavor+arr[start][0], sumCal+arr[start][1]); // 이번꺼 포함하고 다음꺼 가는거
+		
+	}
+}
