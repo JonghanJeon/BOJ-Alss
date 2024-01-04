ac127ce0497e6ffd294dfe410b712e37accf5300
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-04 17:27:32 +0900

    [Bronze III] Title: 소금 폭탄, Time: 148 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String[] arr = br.readLine().split(":");
+        int initHour = Integer.parseInt(arr[0]);
+        int initMin = Integer.parseInt(arr[1]);
+        int initSec = Integer.parseInt(arr[2]);
+
+        int initTime = initHour*3600 + initMin*60 + initSec;
+
+        arr = br.readLine().split(":");
+        int finalHour = Integer.parseInt(arr[0]);
+        int finalMin = Integer.parseInt(arr[1]);
+        int finalSec = Integer.parseInt(arr[2]);
+
+        int finalTime = finalHour*3600 + finalMin*60 + finalSec;
+
+        int ONEDAYTIME = 24*3600;
+
+        int answerHour = 0;
+        int answerMin = 0;
+        int answerSec = 0;
+
+        int resultTime = 0;
+
+        if(finalTime < initTime)
+            resultTime = ONEDAYTIME - initTime + finalTime;
+        else if (finalTime == initTime)
+            resultTime = ONEDAYTIME;
+        else resultTime = finalTime - initTime;
+
+        answerHour = resultTime / 3600;
+        answerMin = (resultTime % 3600) / 60;
+        answerSec = resultTime % 60;
+
+        System.out.printf("%02d:%02d:%02d", answerHour, answerMin, answerSec);
+    }
+}
