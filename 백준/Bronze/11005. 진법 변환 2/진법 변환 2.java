bb10c97ac945968aceffa170d0bb26797ae5edea
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-13 16:57:59 +0900

    [Bronze I] Title: 진법 변환 2, Time: 212 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -13,53 +13,24 @@ public class Main {
 
         StringBuilder sb = new StringBuilder();
 
-        HashMap<Integer, String> map = new HashMap<>(){{
-            put(0, "0");
-            put(1, "1");
-            put(2, "2");
-            put(3, "3");
-            put(4, "4");
-            put(5, "5");
-            put(6, "6");
-            put(7, "7");
-            put(8, "8");
-            put(9, "9");
-            put(10, "A");
-            put(11, "B");
-            put(12, "C");
-            put(13, "D");
-            put(14, "E");
-            put(15, "F");
-            put(16, "G");
-            put(17, "H");
-            put(18, "I");
-            put(19, "J");
-            put(20, "K");
-            put(21, "L");
-            put(22, "M");
-            put(23, "N");
-            put(24, "O");
-            put(25, "P");
-            put(26, "Q");
-            put(27, "R");
-            put(28, "S");
-            put(29, "T");
-            put(30, "U");
-            put(31, "V");
-            put(32, "W");
-            put(33, "X");
-            put(34, "Y");
-            put(35, "Z");
-        }};
+        char [] arr = new char [36];
+        
+        for(char c = '0'; c <= '9' ; c ++){
+            arr[c-'0'] = c;
+        }
+        
+        for(char c = 'A'; c <= 'Z' ; c ++){
+            arr[c-'A'+10] = c;
+        }
 
         while(N > B){
-            sb.append(map.get(N%B));
+            sb.append(arr[N%B]);
             N = N / B;
         }
 
         if(N == B)
             sb.append("01");
-        else sb.append(map.get(N));
+        else sb.append(arr[N]);
 
         System.out.printf(sb.reverse().toString());
     }
