4e8a0ade40f3f65b12738e772e6d3c92e745c604
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-13 17:13:05 +0900

    [Bronze I] Title: 진법 변환 2, Time: 140 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,36 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int B = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
         StringBuilder sb = new StringBuilder();
 
         char [] arr = new char [36];
-        
+
         for(char c = '0'; c <= '9' ; c ++){
             arr[c-'0'] = c;
         }
-        
+
         for(char c = 'A'; c <= 'Z' ; c ++){
             arr[c-'A'+10] = c;
         }
 
-        while(N > B){
+        while(N > 0){
             sb.append(arr[N%B]);
             N = N / B;
         }
-
-        if(N == B)
-            sb.append("01");
-        else sb.append(arr[N]);
 
         System.out.printf(sb.reverse().toString());
     }
