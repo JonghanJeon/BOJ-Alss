5b174c9caad2f90fe34cb80a4c330598b94df2e1
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-13 17:31:20 +0900

    [Silver V] Title: 회문인 수, Time: 172 ms, Memory: 15648 KB -BaekjoonHub

--- 백준/Silver/11068. 회문인 수/회문인 수.java
+++ 백준/Silver/11068. 회문인 수/회문인 수.java
@@ -1,14 +1,16 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         for(int testCase = 1 ; testCase <= N ; testCase ++){
-            int target = sc.nextInt();
+            int target = Integer.parseInt(br.readLine());
             if(check(target))
                 sb.append(1).append("\n");
             else sb.append(0).append("\n");
@@ -20,13 +22,12 @@ public class Main {
     public static boolean check(int target){
         for(int  B = 2 ; B <= 64 ; B ++){
             ArrayList<Integer> list = new ArrayList<>();
-            ArrayList<Integer> list2 = new ArrayList<>();
             int number = target;
             while(number > 0){
                 list.add(number%B);
-                list2.add(number%B);
                 number = number / B;
             }
+            ArrayList<Integer> list2 = new ArrayList<>(list);
             Collections.reverse(list2);
             if(list.equals(list2))
                 return true;
