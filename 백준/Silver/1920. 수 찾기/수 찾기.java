2863490eb97b036344c9c8355776ab841a68d2ed
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-22 16:12:44 +0900

    [Silver IV] Title: 수 찾기, Time: 656 ms, Memory: 48180 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,44 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
-	
-	static int N, M;
-	static int[] arr;
-	static int [] target;
-	
-	public static int bs(int[] arr, int target) {
-		int low = 0;
-		int high = N-1;
-		while(low <= high) {
-			int mid = (low+high)/2;
-			if(arr[mid] == target) return 1;
-			else if(arr[mid] > target) high=mid-1;
-			else low = mid+1;
-		}
-		return 0;
-	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());	
-		st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		target = new int[M];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) target[i] = Integer.parseInt(st.nextToken());
-		
-		Arrays.sort(arr);
-		for(int i=0; i<M; i++) {
-			System.out.println(bs(arr, target[i]));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Set<Integer> set = new TreeSet<>();
+		for (int i = 0; i < N; i++) {
+			set.add(Integer.parseInt(st.nextToken()));
 		}
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++) {
+			if (set.contains(Integer.parseInt(st.nextToken())))
+				sb.append("1").append("\n");
+			else
+				sb.append("0").append("\n");
+		}
+		System.out.println(sb);
 	}
 }
