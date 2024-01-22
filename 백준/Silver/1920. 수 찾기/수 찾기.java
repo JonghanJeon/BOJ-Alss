4630fb5d9be35b5aa8d5581761f206fac9d04ad5
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-22 16:13:59 +0900

    [Silver IV] Title: 수 찾기, Time: 632 ms, Memory: 47500 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -10,7 +10,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		Set<Integer> set = new TreeSet<>();
+		Set<Integer> set = new HashSet<>();
 		for (int i = 0; i < N; i++) {
 			set.add(Integer.parseInt(st.nextToken()));
 		}
