a1b35e3969a67d820996fd7089415fd5466918b3
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-23 17:49:30 +0900

    [Silver II] Title: 용돈 관리, Time: 268 ms, Memory: 22540 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -23,11 +23,15 @@ public class Main {
 		int left = 1;
 		int right = N * 10000;
 		while (left <= right) {
+			// 1 원 부터 최대금액까지 이분탐색
 			int mid = (left + right) / 2;
+			// 근데 이게 무조건 쓸 금액들의 최대 금액보다는 크거나 같아야 뭐가 됨
 			if (mid < max){
 				left = mid + 1;
 			}else {
+				// 이제 인출 횟수를 구하고
 				int count = getCnt(mid);
+				// 최솟값을 구해야 하니까 right를 줄이면서 같을때도 right를 줄여서 left를 반환한다.
 				if(count <= M)
 					right = mid - 1;
 				else left = mid + 1;
