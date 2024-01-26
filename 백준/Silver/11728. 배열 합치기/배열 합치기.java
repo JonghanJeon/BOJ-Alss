59111984364feb7eeee60254e7165575824a637b
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-26 17:21:35 +0900

    [Silver V] Title: 배열 합치기, Time: 1564 ms, Memory: 309452 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		arr = new int[N + M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < M; i++) {
+			arr[N + i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		StringBuilder sb = new StringBuilder();
+		for (int number : arr) {
+			sb.append(number).append(" ");
+		}
+
+		System.out.println(sb);
+	}
+}
