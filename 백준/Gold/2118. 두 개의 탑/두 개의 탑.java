e66cfdd3d3f8ff700cb5fa48c2718c70b027b41c
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-01-26 17:13:27 +0900

    [Gold V] Title: 두 개의 탑, Time: 216 ms, Memory: 19896 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -11,26 +11,30 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		arr = new int[2 * N + 1];
-		int[] tmp = new int[N + 1];
+		arr = new int[N + 1];
 		for (int i = 1; i <= N; i++) {
-			tmp[i] = Integer.parseInt(br.readLine());
-			arr[i] = arr[i - 1] + tmp[i];
+			arr[i] = arr[i - 1] + Integer.parseInt(br.readLine());
 		}
-		for (int i = 1; i <= N; i++) {
-			arr[i + N] = arr[i + N - 1] + tmp[i];
-		}
+		int totalDistance = arr[N];
+		int halfDistance = totalDistance / 2;
 
-		int max = Integer.MIN_VALUE;
-		for (int i = 1; i <= N - 1; i++) {
-			for (int j = i + 1; j <= N; j++) {
-				int distance1 = arr[i + N] - arr[j];
-				int distance2 = arr[j + N] - arr[i + N];
-				int distance = Math.min(distance1, distance2);
-				max = Math.max(distance, max);
+		int left = 1, right = 2;
+		int answer = 0;
+		while (left < right && right <= N) {
+			int distance = arr[right] - arr[left];
+			if (distance < halfDistance) {
+				answer = Math.max(distance, answer);
+				right++;
+			} else if (distance > halfDistance) {
+				distance = totalDistance - distance;
+				answer = Math.max(distance, answer);
+				left++;
+			} else {
+				System.out.println(distance);
+				return;
 			}
 		}
 
-		System.out.println(max);
+		System.out.println(answer);
 	}
 }
