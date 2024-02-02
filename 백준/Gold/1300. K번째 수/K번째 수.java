3a23a7abed40d8b6708f9493c2431f50c7c9c7b0
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-02-02 18:20:33 +0900

    [Gold I] Title: K번째 수, Time: 144 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+	public static int[][] board;
+	public static int[][] prefixSum;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int k = Integer.parseInt(br.readLine());
+
+		int left = 0, right = k;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			//개수
+			int count = 0;
+			for (int i = 1; i <= N; i++) {
+				int div = mid / i;
+				count += Math.min(div, N);
+			}
+			//조절
+			if (k <= count) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+
+		System.out.println(left);
+	}
+}
