594eac7bd048aa55a7ca72a8e5ea923c8440057b
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-02-04 16:06:12 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 392 ms, Memory: 27400 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class Main {
 
 	public static int[][] board;
-	public static int[][] prefixSum;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,47 +14,30 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 
 		board = new int[N + 1][M + 1];
-		prefixSum = new int[N + 1][M + 1];
 		for (int i = 1; i <= N; i++) {
 			String str = br.readLine();
 			for (int j = 1; j <= str.length(); j++) {
 				board[i][j] = Character.getNumericValue(str.charAt(j - 1));
-				prefixSum[i][j] = board[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
 			}
 		}
 
-		int answer = 0;
-		for (int startRow = 1; startRow <= N; startRow++) {
-			for (int startCol = 1; startCol <= M; startCol++) {
-				if (board[startRow][startCol] == 1) {
-					int tmp = 0;
-					while (true) {
-						int defRow = startRow + tmp;
-						int defCol = startCol + tmp;
-						if (N < defRow || M < defCol)
-							break;
-						int result = isPossible(startRow, startCol, defRow, defCol);
-						if (result == -1)
-							break;
-						else
-							answer = Math.max(answer, result);
-						tmp++;
-					}
-				}
+		int[][] dp = new int[N + 1][M + 1];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				if (board[i][j] == 1) {
+					dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+				} else
+					dp[i][j] = 0;
 			}
 		}
 
-		System.out.println(answer);
-	}
+		int max = 0;
+		for (int i = 1; i < N + 1; i++) {
+			for (int j = 1; j < M + 1; j++) {
+				max = Math.max(max, dp[i][j]);
+			}
+		}
 
-	public static int isPossible(int startRow, int startCol, int defRow, int defCol) {
-		int size = (defRow - startRow + 1) * (defCol - startCol + 1);
-		int checkSize =
-			prefixSum[defRow][defCol] - prefixSum[defRow][startCol - 1] - prefixSum[startRow - 1][defCol] + prefixSum[
-				startRow - 1][startCol - 1];
-		if (size == checkSize)
-			return size;
-		else
-			return -1;
+		System.out.println(max * max);
 	}
 }
