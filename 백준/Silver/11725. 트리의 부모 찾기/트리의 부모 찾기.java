8761e43b6c92ba373193e9f8eaccc7474eaddbcc
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-02-13 18:16:54 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 668 ms, Memory: 91628 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,54 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.*;
+
+import javax.swing.plaf.IconUIResource;
 
 public class Main {
 
 	static int N;
+
 	static int[] parent;
-	static boolean[] visited;
+
+	static ArrayList<Integer>[] list, tree;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		ArrayList<Integer>[] arr = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
-			arr[i] = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+
+		parent = new int[N + 1];
+		list = new ArrayList[N + 1];
+		tree = new ArrayList[N + 1];
+
+		for (int i = 0; i < list.length; i++) {
+			list[i] = new ArrayList<>();
+			tree[i] = new ArrayList<>();
 		}
 
 		for (int i = 0; i < N - 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 
-			arr[a].add(b);
-			arr[b].add(a);
+			list[u].add(v);
+			list[v].add(u);
 		}
 
-		parent = new int[N + 1];
-		visited = new boolean[N + 1];
-		Deque<Integer> q = new LinkedList<>();
-
-		q.offer(1);
-		visited[1] = true;
-
-		while (!q.isEmpty()) {
-			int currentNode = q.pollFirst();
-
-			for (int childNode : arr[currentNode]) {
-				if (!visited[childNode]) {
-					visited[childNode] = true;
-					q.offer(childNode);
-					parent[childNode] = currentNode;
-				}
-			}
-		}
-
-		StringBuilder sb = new StringBuilder();
+		makeTree(1, -1);
+		
 		for (int i = 2; i <= N; i++) {
 			sb.append(parent[i]).append("\n");
 		}
+
 		System.out.println(sb);
+	}
+
+	static void makeTree(int currentNode, int parentNode) {
+
+		for (int node : list[currentNode]) {
+			if (node != parentNode) {
+				tree[currentNode].add(node);
+				parent[node] = currentNode;
+				makeTree(node, currentNode);
+			}
+		}
 	}
 }
