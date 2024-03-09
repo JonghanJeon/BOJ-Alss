4c9a167a74181db6ee86d0a875c6a0629c2c5347
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-03-09 16:41:23 +0900

    [Gold IV] Title: 문자열 폭발, Time: 592 ms, Memory: 61660 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import java.io.IOException;
+
+public class Main {
+
+	static Stack<Character> stk = new Stack<>();
+	static String str, bomb;
+
+	public static void main(String[] args) throws IOException {
+		//INPUT
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		str = br.readLine();
+		bomb = br.readLine();
+
+		for (int i = 0; i < str.length(); i++) {
+			stk.push(str.charAt(i));
+			if (stk.size() < bomb.length()) {
+				continue;
+			}
+
+			if (isSame()) {
+				for (int j = 0; j < bomb.length(); j++) {
+					stk.pop();
+				}
+			}
+		}
+
+		if (stk.isEmpty()) {
+			System.out.println("FRULA");
+		} else {
+			StringBuilder sb = new StringBuilder();
+			for (Character c : stk) {
+				sb.append(c);
+			}
+			System.out.println(sb);
+		}
+	}
+
+	static boolean isSame() {
+		for (int i = 0; i < bomb.length(); i++) {
+			if (stk.get(stk.size() - bomb.length() + i) != bomb.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
