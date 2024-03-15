81ff49d01723808c51c92aa93cf43424245e90a5
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-03-15 15:40:15 +0900

    [Gold II] Title: 트리의 지름, Time: 836 ms, Memory: 93456 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,58 +1,85 @@
-// 임의의 노드에서 가장 긴 경로로 연결돼 있는 노드는 트리의 지름에 해당하는 두 노드 중 하나이다.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
+import java.io.IOException;
+
+// 임의의 노드에서 가장 긴 경로로 연결돼 있는 노드는 트리의 지름에 해당하는 두 노드 중 하나이다.
 public class Main {
-	
-	static int N;
-	static ArrayList<Node>[] arrList;
-	static int MAX;
-	static int NODE;
+
+	static int V, maxNode, MAX;
+	static ArrayList<Node>[] nodes;
 	static boolean[] visited;
-	
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);		
-		N = sc.nextInt();
-		
-		arrList = new ArrayList[N+1];
-		MAX = 0;
-		visited = new boolean[N+1];
-		
-		for(int i=1; i<N+1; i++) {
-			arrList[i] = new ArrayList<>();
+	static int[] distance;
+
+	public static void main(String[] args) throws IOException {
+		//INPUT
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		V = Integer.parseInt(br.readLine());
+		nodes = new ArrayList[V + 1];
+		StringTokenizer st;
+		for (int i = 1; i <= V; i++) {
+			nodes[i] = new ArrayList<>();
 		}
-		for(int i=0; i<N; i++) {
-			int now = sc.nextInt();
-			while(true) {
-				int next = sc.nextInt();
-                if(next == -1) break;
-                int weight = sc.nextInt();
-				arrList[now].add(new Node(next, weight));
+		for (int i = 1; i <= V; i++) {
+			st = new StringTokenizer(br.readLine());
+			int currentNode = Integer.parseInt(st.nextToken());
+			while (true) {
+				int node = Integer.parseInt(st.nextToken());
+				if (node == -1) {
+					break;
+				}
+				int weight = Integer.parseInt(st.nextToken());
+				nodes[currentNode].add(new Node(node, weight));
 			}
 		}
-		dfs(1, 0);
+
+		visited = new boolean[V + 1];
+		distance = new int[V + 1];
+		bfs(1);
 		MAX = 0;
-		visited = new boolean[N+1];
-		dfs(NODE,0);
-		System.out.println(MAX);
-	}
-	
-	public static void dfs(int now, int len) {
-		if(len > MAX) {
-			MAX = len;
-			NODE = now;
+		for (int i = 1; i <= V; i++) {
+			if (distance[MAX] < distance[i]) {
+				MAX = i;
+			}
 		}
-		visited[now] = true;
-		for(Node node : arrList[now]) {
-			if(visited[node.next]) continue;
-			dfs(node.next, len+node.w);
+		visited = new boolean[V + 1];
+		distance = new int[V + 1];
+		bfs(MAX);
+		Arrays.sort(distance);
+		System.out.println(distance[V]);
+	}
+
+	static void bfs(final int nowNode) {
+		Queue<Integer> que = new LinkedList<>();
+		que.offer(nowNode);
+		visited[nowNode] = true;
+		while (!que.isEmpty()) {
+			int n = que.poll();
+			for (Node node : nodes[n]) {
+				if (visited[node.getNode()]) continue;
+				visited[node.getNode()] = true;
+				que.offer(node.getNode());
+				distance[node.getNode()] = distance[n] + node.getWeight();
+			}
 		}
 	}
+
 }
 
-class Node{
-	public int next, w;
-	public Node(int next, int w) {
-		this.next = next;
-		this.w = w;
+class Node {
+	public int node, weight;
+
+	public Node(int node, int weight) {
+		this.node = node;
+		this.weight = weight;
+	}
+
+	public int getNode() {
+		return node;
+	}
+
+	public int getWeight() {
+		return weight;
 	}
 }
