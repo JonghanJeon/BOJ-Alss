2b44cd5152e091c4194bc49406a6b51e95d87fa9
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-03-23 09:34:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.80 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String name : participant) {
+            if (map.containsKey(name)) {
+                map.put(name, map.get(name) + 1);
+            } else {
+                map.put(name, 1);
+            }
+        }
+        
+        for (String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+        
+        String answer = "";
+        
+        for (String name : map.keySet()) {
+            if (map.get(name) != 0) {
+                answer = name;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
