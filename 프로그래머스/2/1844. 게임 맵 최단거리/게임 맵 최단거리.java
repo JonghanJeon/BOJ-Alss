a1a6348c4a5ad84933ac0b9f7ae45e946c9bc51e
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-09-27 19:36:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.26 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,92 +1,55 @@
 import java.util.*;
 
+class Node {
+    int row;
+    int col;
+    int count;
+    
+    Node(int row, int col, int count) {
+        this.row = row;
+        this.col = col;
+        this.count = count;
+    }
+}
+
 class Solution {
     
-    public int[][] board;
-    public boolean[][] visited;
-    public int answer = -1;
-    public int[] dr = {-1, 1, 0, 0}; //상하
-    public int[] dc = {0, 0, -1, 1}; //좌우
+    public int[] dr = {-1, 1, 0, 0}; //row 상 하 좌 우
+    public int[] dc = {0, 0, -1, 1}; //col 상 하 좌 우
     
     public int solution(int[][] maps) {
-        board = maps;
-        visited = new boolean[maps.length][maps[0].length];
-        // dfs(0, 0, 1);
-        // if (answer == Integer.MAX_VALUE) {
-        //     return -1;
-        // } else {
-        //     return answer;
-        // }
-        bfs(0, 0);
-        return answer;
-        
-    }
-    
-    public void bfs(int row, int col) {
+        int answer = -1;
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
         Queue<Node> que = new LinkedList<>();
-        
         que.offer(new Node(0, 0, 1));
         visited[0][0] = true;
-        
-        while (!que.isEmpty()) {
+        while(!que.isEmpty()) {
             Node cur = que.poll();
             
-            if (cur.row == board.length - 1 && cur.col == board[0].length - 1) {
+            if (cur.row == maps.length - 1 &&
+                cur.col == maps[0].length - 1) {
                 answer = cur.count;
-                return;
+                break;
             }
             
-            for (int i = 0; i < 4; i++) {
+            for(int i = 0; i < 4; i++) {
                 int newRow = cur.row + dr[i];
                 int newCol = cur.col + dc[i];
-                if (0 <= newRow && newRow < board.length && 0 <= newCol && newCol < board[0].length) {
-                    if (visited[newRow][newCol] == false && board[newRow][newCol] == 1) {
+                
+                if (0 <= newRow && newRow < maps.length &&
+                    0 <= newCol && newCol < maps[0].length) {
+                    
+                    if (maps[newRow][newCol] == 1 &&
+                        !visited[newRow][newCol]) {
                         visited[newRow][newCol] = true;
-                        que.offer(new Node(newRow, newCol, cur.count + 1));
+                        Node nextNode = new Node(newRow, newCol, cur.count + 1);
+                        que.offer(nextNode);
                     }
                 }
             }
+            
         }
+        
+        return answer;
     }
-    
-    public class Node {
-        int row;
-        int col;
-        int count;
-        
-        public Node(int row, int col, int count) {
-            this.row = row;
-            this.col = col;
-            this.count = count;
-        }
-    }
-    
-//     public void dfs(int row, int col, int sum) {
-//         if (answer <= sum) {
-//             return;
-//         }
-        
-//         if (row == board.length - 1 && col == board[0].length - 1) {
-//             answer = Math.min(answer, sum);
-//             return;
-//         }
-        
-//         board[row][col] = 0;
-        
-//         //우하좌상
-//         int[] dr = {0, 1, 0, -1}; 
-//         int[] dc = {1, 0, -1, 0}; 
-//         for (int i = 0; i < 4; i++) {
-//             int newRow = row + dr[i];
-//             int newCol = col + dc[i];
-//             if (0 <= newRow && newRow < board.length && 0 <= newCol && newCol < board[0].length) {
-//                if (board[newRow][newCol] == 1) {
-//                    dfs(newRow, newCol, sum + 1);
-//                } 
-//             }
-//         }
-        
-        
-//         board[row][col] = 1;
-//     }
 }
