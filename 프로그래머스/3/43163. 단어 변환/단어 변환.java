8e00d59fdaccade69389cc35a8093fd96bc3e3b5
JongHan Jeon <117141827+JonghanJeon@users.noreply.github.com>
2024-10-26 23:57:48 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,53 @@
 import java.util.*;
 
-class Word {
-    String word;
-    int count;
-    
-    Word(String word, int count) {
-        this.word = word;
-        this.count = count;
-    }
-}
-
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+    
+    class Word {
+        String str;
+        int count;
         
-        Queue<Word> que = new LinkedList<>();
+        Word(String str, int count) {
+            this.str = str; this.count = count;
+        }
+    }
+    
+    int answer = Integer.MAX_VALUE;
+    
+    String[] words;
+    
+    public int solution(String begin, String target, String[] a) {
+        
+        words = a;
         boolean[] visited = new boolean[words.length];
-        que.offer(new Word(begin, 0));
+        dfs(begin, 0, target, visited);
         
+        if (answer == Integer.MAX_VALUE) return 0;
+        return answer;
         
-        while(!que.isEmpty()) {
-            Word cur = que.poll();
-            
-            if (cur.word.equals(target)) {
-                answer = cur.count;
-                break;
-            }
-            
-            for (int i = 0; i <words.length; i++) {
-                int cnt = 0;
-                String word = words[i];
-                for (int j = 0; j < cur.word.length(); j++) {
-                    if (cur.word.charAt(j) != word.charAt(j)) cnt++;
-                }
-                
-                if (cnt == 1 && !visited[i]) {
-                    visited[i] = true;
-                    que.offer(new Word(word, cur.count + 1));
-                }
-            }
+    }
+    
+    public void dfs(String cur, int count, String target, boolean[] visited) {
+        if (cur.equals(target)) {
+            answer = Math.min(answer, count);
+            return;
         }
         
-        return answer;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(cur)) continue;
+            if (visited[i]) continue;
+            
+            int diff = 0;
+            for (int j = 0; j < cur.length(); j++) {
+                int c1 = cur.charAt(j);
+                int c2 = words[i].charAt(j);
+                if (c1 != c2) diff++;
+            }
+            
+            if (diff != 1) continue;
+            
+            visited[i] = true;
+            dfs(words[i], count + 1, target, visited);
+            visited[i] = false;
+        }
     }
 }
